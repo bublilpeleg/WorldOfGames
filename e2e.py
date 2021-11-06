abe6b7a694cea08bfcb9cfe56f038a31dec6462f
pelegb <pelegbub@gmail.com>
2021-11-06 12:42:32 +0200

    e

--- e2e.py
+++ e2e.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.chrome.service import Service
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 # s=Service("/app/chromedriver_linux64/chromedriver")
 # driver = webdriver.Chrome(service=s)
 
-driver = webdriver.Chrome(executable_path="/app/chromedriver_linux64/chromedriver")
+# driver = webdriver.Chrome(executable_path="/app/chromedriver_linux64/chromedriver")
 
 
 # def test_scores_service(url):
@@ -15,12 +15,12 @@ driver = webdriver.Chrome(executable_path="/app/chromedriver_linux64/chromedrive
 #     my_url = driver.find_element_by_id("score")
 #     return int(my_url) in range(1, 100)
 def test_scores_service():
-    # svc = Service(ChromeDriverManager().install())
-    # svc = Service("/app/chromedriver_linux64/chromedriver")
-    # my_driver = webdriver.Chrome(service=svc)
-    driver.maximize_window()
-    driver.get("http://127.0.0.1:8777/")
-    score = driver.find_element(By.ID, "score").text
+    svc = Service(ChromeDriverManager().install())
+    svc = Service("/app/chromedriver_linux64/chromedriver")
+    my_driver = webdriver.Chrome(service=svc)
+    my_driver.maximize_window()
+    my_driver.get("http://127.0.0.1:8777/")
+    score = my_driver.find_element(By.ID, "score").text
     if 1 < int(score) < 1000:
         return True
     else:
