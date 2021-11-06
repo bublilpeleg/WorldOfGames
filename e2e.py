cfd4cc5e6d2e17da94121e8126b96613fc5c3b23
pelegb <pelegbub@gmail.com>
2021-11-06 11:46:22 +0200

    :

--- e2e.py
+++ e2e.py
@@ -15,7 +15,8 @@ from selenium.webdriver.common.by import By
 #     my_url = driver.find_element_by_id("score")
 #     return int(my_url) in range(1, 100)
 def test_scores_service():
-    svc = Service(ChromeDriverManager().install())
+    # svc = Service(ChromeDriverManager().install())
+    svc = Service("/app/chromedriver_linux64/chromedriver")
     my_driver = webdriver.Chrome(service=svc)
     my_driver.maximize_window()
     my_driver.get("http://127.0.0.1:8777/")
@@ -27,7 +28,7 @@ def test_scores_service():
 
 
 def main_function():
-    return 0 if test_scores_service("http://127.0.0.1:8777/") else -1
+    return 0 if test_scores_service() else -1
 
 
 main_function()
