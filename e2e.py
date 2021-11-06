9276407aa7b89330f4f0284d2e7fb84836e5fd79
pelegb <pelegbub@gmail.com>
2021-11-06 13:24:27 +0200

    lets try it

--- e2e.py
+++ e2e.py
@@ -2,12 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+
+
 # from selenium.webdriver.chrome.service import Service
 #
 # s=Service("/app/chromedriver_linux64/chromedriver")
 # driver = webdriver.Chrome(service=s)
 
-my_driver = webdriver.Chrome(executable_path="/chromedriver_linux64/chromedriver")
+# my_driver = webdriver.Chrome(executable_path="/chromedriver_linux64/chromedriver")
 
 
 # def test_scores_service(url):
@@ -15,10 +17,10 @@ my_driver = webdriver.Chrome(executable_path="/chromedriver_linux64/chromedriver
 #     my_url = driver.find_element_by_id("score")
 #     return int(my_url) in range(1, 100)
 def test_scores_service():
-    # svc = Service(ChromeDriverManager().install())
+    s = Service(ChromeDriverManager().install())
     # svc = Service("/app/chromedriver_linux64/chromedriver")
-    # my_driver = webdriver.Chrome(service=svc)
-    # my_driver.maximize_window()
+    my_driver = webdriver.Chrome(service=s)
+    my_driver.maximize_window()
     my_driver.get("http://127.0.0.1:8777/")
     score = my_driver.find_element(By.ID, "score").text
     if 1 < int(score) < 1000:
