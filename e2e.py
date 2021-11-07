0548297fe4e771ba8f89360a17e40778291b209f
pelegb <pelegbub@gmail.com>
2021-11-07 10:05:20 +0200

    peleg

--- e2e.py
+++ e2e.py
@@ -2,32 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 
 
-# from selenium.webdriver.chrome.service import Service
-#
-# s=Service("/app/chromedriver_linux64/chromedriver")
-# driver = webdriver.Chrome(service=s)
-
-my_driver = webdriver.Chrome(executable_path="/app/chromedriver_linux64/chromedriver")
-
-
-# def test_scores_service(url):
-#     driver.get(url)
-#     my_url = driver.find_element_by_id("score")
-#     return int(my_url) in range(1, 100)
 def test_scores_service():
-    # chrome_options = Options()
-    # chrome_options.add_argument("--headless")
-    # chrome_options.add_argument('--no-sandbox')
-    # my_driver = webdriver.Chrome('/app/chromedriver_linux64/chromedriver', options=chrome_options)
-    # driver.get('http://www.google.com')
-    print('test')
-    # s = Service(ChromeDriverManager().install())
-    # svc = Service("/app/chromedriver_linux64/chromedriver")
-    # my_driver = webdriver.Chrome(service=s)
-    # my_driver.maximize_window()
+    svc = Service(ChromeDriverManager().install())
+    my_driver = webdriver.Chrome(service=svc)
+    my_driver.maximize_window()
     my_driver.get("http://127.0.0.1:8777/")
     score = my_driver.find_element(By.ID, "score").text
     if 1 < int(score) < 1000:
@@ -37,7 +17,11 @@ def test_scores_service():
 
 
 def main_function():
-    return 0 if test_scores_service() else -1
+    test_score = test_scores_service()
+    if test_score:
+        exit(0)
+    else:
+        exit(-1)
 
 
 main_function()
